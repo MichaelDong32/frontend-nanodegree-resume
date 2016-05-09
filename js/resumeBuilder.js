7b95c3b83a2859d4365b1cd2a43b7c44b1b0934c
MichaelDong32 <mjdong32@gmail.com>
2016-05-09 22:37:14 +1200

    Udacity lesson 1, resume header quiz'

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,36 @@
 /*
 This is empty on purpose! Your code to build the resume will go here.
  */
+
+ // $("#main").append("Michael");
+
+ // [string].replace([old], [new]);
+
+/*var awesomeThoughts = ('My name is Michael and I am AWESOME!')
+
+console.log(awesomeThoughts)
+
+var email = ('michaeldong@gmail.com')
+
+var newEmail =
+  email.replace('michaeldong', 'michaeldong32')
+
+console.log(email)
+console.log(newEmail)
+
+var funThoughts =
+    awesomeThoughts.replace('AWESOME', 'FUN')
+
+console.log(funThoughts)
+
+$("#main').append(funThoughts); 
+*/
+
+var formattedName = HTMLheaderName.replace('%data%',name);
+
+var name = "Michael Dong";
+var role = "Web Developer";
+var formattedRole = HTMLheaderRole.replace('%data%',role);
+
+$("#header").prepend(formattedRole);
+$("#header").prepend(formattedName);
