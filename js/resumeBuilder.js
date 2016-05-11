ae2c6f6da5e1c1ad294b860f1b459c131c8c72cd
MichaelDong32 <mjdong32@gmail.com>
2016-05-11 13:55:42 +1200

    resume builder

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -2,7 +2,7 @@
 This is empty on purpose! Your code to build the resume will go here.
  */
 
- // $("#main").append("Michael");
+ $("#main").append("Michael");
 
  // [string].replace([old], [new]);
 
@@ -26,11 +26,38 @@ console.log(funThoughts)
 $("#main').append(funThoughts); 
 */
 
-var formattedName = HTMLheaderName.replace('%data%',name);
+/*var formattedName = HTMLheaderName.replace('%data%',name);
 
 var name = "Michael Dong";
 var role = "Web Developer";
 var formattedRole = HTMLheaderRole.replace('%data%',role);
 
-$("#header").prepend(formattedRole);
-$("#header").prepend(formattedName);
+$("#header").append(formattedRole);
+$("#header").prepend(formattedName); 
+
+var bio = {
+  "name": "Michael Dong",
+  "role": "Junior Software Developer",
+  "email": "mjdong32@gmail.com",
+  "phone": "0272332823",
+  "location": "Wellington"
+  "welcomeMessage": "Hello and check out my awesome resume.",
+  "skills": ["communicator", "learner", "good guy", "works hard", "delivers on time", "team player", "coach"],
+  "bioPic": "images/fry.jpg"
+
+}
+
+var formattedName = HTMLheaderName.replace('%data%', bio.name)
+
+$("#main").prepend(formattedName);
+
+
+/*var picture = HTMLbioPic.replace('%data%',bio.bioPic);
+var message = HTMLwelcomeMsg.replace('%data%',bio.welcomeMessage);
+
+$("#header").append(picture);
+$("#header").append(message);
+
+var skills = HTMLskills.replace('%data%',bio.skills);
+$("#header").append(skills);
+
